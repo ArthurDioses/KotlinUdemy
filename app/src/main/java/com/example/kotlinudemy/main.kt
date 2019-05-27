7fbb3a6f207a080ceca46bf64d350418dc4454b8
Arthur Dioses <adioses93@gmail.com>
2019-05-27 00:16:03 -0500

    impl(): -Configuracion del gradle -Clase 6 - Variables

---
+++ app/src/main/java/com/example/kotlinudemy/main.kt
@@ -0,0 +1,23 @@
+package com.example.kotlinudemy
+
+
+
+fun main(args: Array<String>) {
+    println("Hello fucking World")
+
+    var epicVar = "Dioses"
+    var num1 = 5
+    val num2 = 67
+
+    println(epicVar)
+    println(num1)
+    println(num2)
+
+    epicVar = "IronMan"
+    num1 = 100
+    //num2 = 99
+
+    println(epicVar)
+    println(num1)
+    println(num2)
+}
