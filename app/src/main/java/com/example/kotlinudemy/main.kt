8ba775e1ee9c299d3164d9a555feda29713f15b8
Arthur Dioses <adioses93@gmail.com>
2019-06-09 20:27:57 -0500

    impl(): -Usando break statement

--- app/src/main/java/com/example/kotlinudemy/main.kt
+++ app/src/main/java/com/example/kotlinudemy/main.kt
@@ -350,7 +350,7 @@ fun main(args: Array<String>) {
         println("Never ending loop")
     }
 }*/
-fun main(args: Array<String>) {
+/*fun main(args: Array<String>) {
     var x = 100
     println("While Loop")
 
@@ -365,4 +365,21 @@ fun main(args: Array<String>) {
         println(x)
         x++ // x += 1 or x = x + 1
     } while (x <= 10)
+}*/
+fun main(args: Array<String>) {
+    var i = 0
+
+    loop@ while (i < 5) {
+        var x = 0
+        while (x < 10) {
+            if (x == 4) {
+                //break
+                break@loop
+            }
+            println("i is $i and x is $x")
+            x++
+        }
+        i++
+    }
+
 }
