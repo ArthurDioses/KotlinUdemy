9cf09df4ae8ebe86b0bac2edff2a9c5b340c105b
Arthur Dioses <adioses93@gmail.com>
2019-06-17 00:42:09 -0500

    impl(): -Bloques de multiples try catch

--- app/src/main/java/com/example/kotlinudemy/main.kt
+++ app/src/main/java/com/example/kotlinudemy/main.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinudemy
 
+import java.lang.Exception
+
 
 /*
 fun main(args: Array<String>) {
@@ -493,7 +495,7 @@ inline fun inlineFunc(myFunc: () -> Unit) {
 
 
 }*/
-fun main(args: Array<String>) {
+/*fun main(args: Array<String>) {
     var string = Convert("p")
     println(string)
 }
@@ -505,4 +507,24 @@ fun Convert(strVar: String): Int {
         0
     }
 }
+*/
+fun main(args: Array<String>) {
+    val num1 = 100
+    val num2 = 20
+    var arr = IntArray(10)
+    try {
+        arr[10] = num1 / num2
+        //println(arr[9])
+    } catch (error: ArithmeticException) {
+        println(error)
 
+    //} catch (error: ArrayIndexOutOfBoundsException) {
+     ///   println(error)
+    } catch (error: Exception) {
+        println("General Error")
+        println(error)
+    }
+    for (value in arr) {
+        println(value)
+    }
+}
