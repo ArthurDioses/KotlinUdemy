2e83242dae2295af883d5cb8137fc03a350e3792
Arthur Dioses <adioses93@gmail.com>
2019-06-25 22:58:37 -0500

    impl(): -Operador Elvis

--- app/src/main/java/com/example/kotlinudemy/main.kt
+++ app/src/main/java/com/example/kotlinudemy/main.kt
@@ -597,8 +597,20 @@ fun Convert(strVar: String): Int {
     }
     println(epicVar)
 }*/
-fun main(args:Array<String>) {
+/*fun main(args:Array<String>) {
     var epic:Any? = null
     var string:String? = epic as? String
     println(string)
+}*/
+fun main(args: Array<String>) {
+    var string1: String? = null
+    var string2: String? = "Hello World"
+
+    //var length1: Int = if (string1 != null) string1.length else -1
+    //var length2: Int = if (string2 != null) string2.length else -1
+
+    var length1: Int = string1 ?.length ?: -1
+    var length2: Int = string2 ?.length ?: -1
+    println(length1)
+    println(length2)
 }
